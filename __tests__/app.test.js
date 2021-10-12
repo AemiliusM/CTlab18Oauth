5eafd60c186171c059db7529b26d29baaaf28c0d
Simon Kaine <simonkaine@gmail.com>
2021-10-12 16:33:09 -0700

    Red failing test: Should GET /api/v1/auth/login and redirect to github for login.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,6 +8,17 @@ describe('CTlab18OAuth routes', () => {
     return setup(pool);
   });
 
+  it('Should GET /api/v1/auth/login and redirect to github for login', async () => {
+    const response = await request(app).get('/api/auth/login');
+
+    expect(response.body).toEqual({
+      login: 'fake_user',
+      avatar: 'github_avatar_url', 
+      // iat: Date.now(),
+      // exp: Date.now(),
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });

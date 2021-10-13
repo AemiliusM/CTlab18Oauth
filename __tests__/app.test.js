861e4ae00d5d7dc6a5a882c6422d2a81a90ddcb7
Simon Kaine <simonkaine@gmail.com>
2021-10-13 14:19:38 -0700

    Red failing test: Should POST to create a new user post.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,6 +15,12 @@ jest.mock('../lib/middleware/ensureAuth.js', () => {
   };
 });
 
+const postUser = {
+  userId: '1',
+  photoUrl: 'https://example.com/image.png',
+  caption: 'This some lovely caption from the bottom of my heart :)',
+};
+
 describe('CTlab18OAuth routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -29,9 +35,22 @@ describe('CTlab18OAuth routes', () => {
       iat: expect.any(Number),
       exp: expect.any(Number),
     });
-  
+  });
 
-    
+  it('Should POST to create a new user post', async () => {
+    const response = await request(app).get('/api/auth/me');
+    await request(app).post('/api/auth/posts').send(postUser);
+
+    expect(response.body).toEqual({
+      id: '1',
+      login: 'fake_user',
+      avatar: 'https://example.com/image.png',
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+      userId: expect.any(String),
+      photoUrl: expect.any(String),
+      caption: expect.any(String),
+    });
   });
 
   afterAll(() => {

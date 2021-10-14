ea0baac15b536d6ab61e5e0f54e6117d214460b3
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-10-13 17:22:36 -0700

    green passing post new user post

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,14 +2,14 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+const { post } = require('superagent');
+const User = require('../lib/models/User-model.js');
 
 jest.mock('../lib/middleware/ensureAuth.js', () => {
   return (req, res, next) => {
     req.user = {
       login: 'fake_user',
       avatar: 'https://example.com/image.png',
-      iat: Date.now(),
-      exp: Date.now(),
     };
     next();
   };
@@ -27,14 +27,16 @@ describe('CTlab18OAuth routes', () => {
   });
 
   it('Should GET /api/auth/me -- users profile page ', async () => {
+    await User.insert({
+      login: 'fake_user',
+      avatar: 'https://example.com/image.png',
+    });
     const response = await request(app).get('/api/auth/me');
 
     expect(response.body).toEqual({
       id: '1',
       login: 'fake_user',
       avatar: 'https://example.com/image.png',
-      iat: expect.any(Number),
-      exp: expect.any(Number),
     });
   });
 
@@ -42,18 +44,16 @@ describe('CTlab18OAuth routes', () => {
   // then call a post request 
   
   it('Should POST to create a new user post', async () => {
-    await request(app).get('/api/auth/me');
+    await User.insert({
+      login: 'fake_user',
+      avatar: 'https://example.com/image.png',
+    });
     return await request(app).post('/api/auth/posts').send(postUser)
       .then((res) => {
         expect(res.body).toEqual({
+          ...postUser,
           id: '1',
-          login: 'fake_user',
-          avatar: 'https://example.com/image.png',
-          iat: expect.any(Number),
-          exp: expect.any(Number),
-          userId: expect.any(String),
-          photoUrl: expect.any(String),
-          caption: expect.any(String),
+          
         });
       });
   });
